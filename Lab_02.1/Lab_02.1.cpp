c83afb900ca0154fd76f0af6e32a572275cac069
Andrii <andrey.warspa@gmail.com>
2026-09-19 21:28:48 +0300

    master remote change

--- Lab_02.1/Lab_02.1.cpp
+++ Lab_02.1/Lab_02.1.cpp
@@ -12,7 +12,7 @@ int main()
 	double a; // Вхідний параметр
 	double z1;// Результат обчислення 1-го виразу
 	double z2;// Результат обчислення 2-го виразу
-	cout << "Enter a="; cin >> a;
+	cout << "Pleasy Enter a="; cin >> a;
 	z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
 	z2 = 4 * cos(a / 2) * cos((5.0 / 2) * a) * cos(4 * a);
 	cout << "z1=" << z1 <<'\n';
